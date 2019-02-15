e221c50a2e5696479800f4cdfc1fb7d44680dab0
desarkarJishnu <desarkarjishnu@gmail.com>
2019-02-15 20:19:27 +0530

    Notification list view, send email

--- app/src/main/java/com/example/user/appbijatraining/NotificationsFragment.java
+++ app/src/main/java/com/example/user/appbijatraining/NotificationsFragment.java
@@ -7,12 +7,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
+
+    List<NotificationList> notificationLists;
+    ListView listView;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notifications, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        notificationLists = new ArrayList<>();
+
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+        notificationLists.add(new NotificationList("Dummy notification 1"));
+
+
+        listView = getView().findViewById(R.id.listview_notification);
+
+        NotificationListAdapter adapter = new NotificationListAdapter(getContext(), R.layout.custom_notification_list_item, notificationLists);
+
+        listView.setAdapter(adapter);
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/user/appbijatraining/SendFragment.java
+++ app/src/main/java/com/example/user/appbijatraining/SendFragment.java
@@ -7,12 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendFragment extends Fragment {
+
+    EditText emailText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_send, container, false);
     }
+
+
 }
