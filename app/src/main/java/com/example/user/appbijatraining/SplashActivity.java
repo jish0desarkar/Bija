016e02ce96d44924b29f76660c784e14dccdeb43
desarkarJishnu <desarkarjishnu@gmail.com>
2019-03-26 19:15:11 +0530

    Testing

--- app/src/main/java/com/example/user/appbijatraining/SplashActivity.java
+++ app/src/main/java/com/example/user/appbijatraining/SplashActivity.java
@@ -14,6 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        System.out.println("Testing");
         new Handler().postDelayed(new Runnable() {
 
             /*
